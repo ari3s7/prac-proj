c1d092b6df14ea2b77ccbd0f5c8f4c4fdfa10d52
Abhishek Sharma <abhi761685@gmail.com>
2025-10-15 13:42:51 +0530

    added MongoDB connetion

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema()
+mongoose.connect(process.env.MONGO_URI)
+const Schema = mongoose.Schema;
 const  ObjectId = mongoose.ObjectId;
 
 
